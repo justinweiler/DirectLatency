959b749866a77e4396660f922eb4274ccecf2e48
Justin Weiler <Justin.Weiler@audiencescience.com>
2015-09-23 16:53:41 -0700

    config

--- config.js
+++ config.js
@@ -1,15 +1,12 @@
-module.exports.out          = '/tmp/latency.log';
-module.exports.port         = 3000;
-module.exports.host         = 'http://localhost';
+module.exports.out          = '/mnt/metrics/latency.log';
+module.exports.port         = 80;
+module.exports.host         = 'http://52.26.78.96/cnc/status';
 module.exports.url          = module.exports.host;
 module.exports.datacenterId = '0'; // single character 0-f
 
 module.exports.datacenters = [
-    'West Coast|http://52.26.78.96/cnc/status',
-    'East Coast|http://52.26.78.96/cnc/status',
-    'Singapore|http://52.26.78.96/cnc/status',
-    'South America|http://52.26.78.96/cnc/status',
-    'Oort Cloud|http://52.26.78.96/cnc/status'
+    'Oregon|http://52.26.78.96/cnc/status',
+    'Singapore|http://54.254.201.180/cnc/status'
 ];
 
 if (typeof module.exports.port === 'number')
